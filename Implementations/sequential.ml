4843df1925b7840effa4bbf1b6ae52512b1f3d7d
axeldavy <axel.davy@ens.fr>
2013-05-20 19:49:49 +0200

    Almost correct sequential implementation

--- Implementations/sequential.ml
+++ Implementations/sequential.ml
@@ -3,7 +3,7 @@
 open Unix
 open Kahn
 open List
-
+exception Stop 
 (* Constant *******************************************************************)
 
 let trace_enable = true
@@ -27,29 +27,39 @@ let trace =
 
 module S : S =
 struct
-  type 'a process = ('a -> unit) -> unit
-  type 'a in_port = in_channel
-  type 'a out_port = out_channel
+  type 'a answer = SUCCESS | FAILURE
+  type 'a process = ('a -> unit) -> 'a answer
+  type 'a in_port = 'a Queue.t
+  type 'a out_port = 'a Queue.t
+  
   
   type 'a reponse = REPONSE of 'a 
   let () = Random.self_init () 
   
   let new_channel () =
     trace "new_channel";
-    let (fd_in, fd_out) = pipe () in
-    (in_channel_of_descr fd_in, out_channel_of_descr fd_out)
+    let q = Queue.create () in
+      q, q
   
   let put v c f =
     trace "put";
-    Marshal.to_channel c (v : 'a) [ Marshal.Closures ];
-    f ()
+    Queue.push v c;
+    f ();
+    SUCCESS
   
-  let rec get c f = (* problème si get est bloquant *)
+  let rec get c f = (* problème si get est bloquant ? *)
     trace "get";
-    let v = ((Marshal.from_channel c) : 'a) in
-    f v
-  
-(* idée d'amélioration: 
+    try
+      let v = Queue.pop c in
+      f v;
+      SUCCESS 
+    with
+    Queue.Empty ->
+      FAILURE
+      
+      (*
+      
+      * idée d'amélioration: 
 actuellement les fonctions données à put et get sont les fonctions fill données par bind et run.
 doco pourrait appeller get et put avec une autre fonction, qui en cas de succès de get/put appelle la fonction de départ (fill) avec le bon argument
 et si get échoue, ne pas appeler la fonction de départ, mais réappeler get plus tard.
@@ -58,34 +68,62 @@ TODO pour cette méthode: remplacer l'implémentation des channel par c une fonc
 Je n'ai pas le temps là, peut-être plus tard.
 
 *)  
-  
+
+
+
   let rec doco l = (* doco très imparfait *)
     trace "doco";
     match l with
     | [] -> failwith "doco on an empty list"
-    | [f] -> f
-    | a::q -> if(Random.bool ())                  
-               then  (fun v -> a v;(doco q) v)
-               else  (fun v -> (doco q) v; a v)
-  
-  let return v f=
+    | [f] -> trace "fin doco";f
+    | a::q -> 
+                 (fun s -> 
+		       let l = ref([]) in
+		       let doco_fill v = l:= [REPONSE (v)] in
+		       match (a doco_fill) with
+		       | SUCCESS -> (match (hd(!l)) with
+				    |REPONSE (v) -> s v;
+				    (doco q) s)
+		       | FAILURE -> (doco (q@[a])) s
+               
+		)
+  let return v (f:('a->unit))=
     trace "return";
-    f v
-    
+    f v;
+    SUCCESS
   
-  let bind e e' f=
+  
+  let bind (e:'a process) e'=
     trace "bind";
-    let l = ref([])in
-    let fill v = l:= [REPONSE (v)] in
-    e fill;
-    match (hd(!l)) with
-    |REPONSE (v) -> e' v f (* allow to have v = () *)
+    let rec calc_e = ref (fun () ->
+		let l = ref([])in
+		let fill v = l:= [REPONSE (v)] in
+		let rec loop_until_success () =
+		match (e fill) with
+		|SUCCESS -> ()
+		|FAILURE -> loop_until_success() 
+		in
+		loop_until_success(); 
+		match (hd(!l)) with
+		|REPONSE (v) -> calc_e:=(fun () -> v);raise Stop;v) in
+    (
+      fun f -> 
+      try 
+        let v = ((!calc_e)()) in 
+        e' v f
+      with Stop -> FAILURE )
+     
   
   let run e =
     trace "run";
     let l = ref([]) in
-    let fill v = l:= [v] in
-    e fill;
-    hd (!l)
-       
+    let fill v = l:= [REPONSE(v)] in
+    let rec loop_until_success () =
+      match(e fill) with
+      |SUCCESS -> ()
+      |FAILURE -> loop_until_success() 
+      in
+      loop_until_success();
+       match (hd(!l)) with
+    |REPONSE (v) ->  v
 end
